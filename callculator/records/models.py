8afb994e7ba92905567c3d8e6ac8e141624de1cd
Diego Ponciano <dgo.ponciano@gmail.com>
2018-03-16 01:44:49 -0300

    Call charge calculation, considering reduced tariff

--- callculator/records/models.py
+++ callculator/records/models.py
@@ -1,6 +1,11 @@
+from datetime import timedelta
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
+
+MINUTELY_PRICE = 0.09
+STANDING_CHARGE = 0.36
 
 
 class CallRecord(models.Model):
@@ -10,9 +15,41 @@ class CallRecord(models.Model):
     source = models.CharField(max_length=20)
     destination = models.CharField(max_length=20)
     duration = models.DurationField(null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+
+    def calculate_duration(self):
+        self.duration = self.ended_at - self.started_at
+
+    def standard_minutes(self):
+        minutes = 0
+        current = self.started_at
+        end = self.ended_at
+
+        while current < end:
+            day_start = current.replace(hour=6, minute=0, second=0,
+                                        microsecond=0)
+            day_end = current.replace(hour=22, minute=0, second=0,
+                                      microsecond=0)
+            if current >= day_end:
+                current = current.replace(hour=0, minute=0, second=0,
+                                          microsecond=0) + timedelta(1)
+                continue
+            if end < day_end:
+                day_end = end
+            if current > day_start:
+                day_start = current
+            minutes += (day_end - day_start).seconds // 60
+            current = current.replace(hour=0, minute=0, second=0,
+                                      microsecond=0) + timedelta(1)
+        return minutes
+
+    def calculate_price(self):
+        self.price = (
+            STANDING_CHARGE + (self.standard_minutes() * MINUTELY_PRICE))
 
 
 @receiver(pre_save, sender=CallRecord)
-def calculate_duration(sender, instance, **kwargs):
+def calculate_price_and_duration(sender, instance, **kwargs):
     if instance.started_at and instance.ended_at:
-        instance.duration = instance.ended_at - instance.started_at
+        instance.calculate_duration()
+        instance.calculate_price()
--- tests/records/test_models.py
+++ tests/records/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 from dateutil.parser import parse
 from datetime import timedelta
 
@@ -16,7 +17,60 @@ class TestRecord:
     def test_calculates_duration(self):
         start = parse('01-01-2018 16:00Z')
         end = parse('01-01-2018 16:30Z')
-        record = CallRecord(started_at=start, ended_at=end)
+        record = CallRecord(started_at=start, ended_at=end,
+                            call_id=uuid.uuid4())
         record.save()
 
         assert record.duration == timedelta(minutes=30)
+
+
+@pytest.mark.django_db
+class TestStardardMinutesCalculation:
+    def test_standard_minutes_at_same_day(self):
+        start = parse('01-01-2018 21:57:13Z')
+        end = parse('01-01-2018 22:10:56Z')
+        record = CallRecord(started_at=start, ended_at=end,
+                            call_id=uuid.uuid4())
+
+        assert record.standard_minutes() == 2
+
+        record.started_at = parse('01-01-2018 21:00:00Z')
+        assert record.standard_minutes() == 60
+
+    def test_standard_minutes_on_two_days(self):
+        start = parse('01-01-2018 21:57:13Z')
+        end = parse('01-02-2018 6:10:56Z')
+        record = CallRecord(started_at=start, ended_at=end,
+                            call_id=uuid.uuid4())
+
+        assert record.standard_minutes() == 12
+
+
+@pytest.mark.django_db
+class TestPricingRules:
+    def test_reduced_tariff_charges_only_standing_charge(self):
+        start = parse('01-01-2018 22:57:13Z')
+        end = parse('01-01-2018 23:10:56Z')
+        record = CallRecord(started_at=start, ended_at=end,
+                            call_id=uuid.uuid4())
+        record.save()
+
+        assert record.price == 0.36
+
+    def test_standard_call_charges_standing_and_minutely(self):
+        start = parse('01-01-2018 20:55:13Z')
+        end = parse('01-01-2018 20:58:56Z')
+        record = CallRecord(started_at=start, ended_at=end,
+                            call_id=uuid.uuid4())
+        record.save()
+
+        assert record.price == 0.36 + 0.09*3
+
+    def test_mixed_standard_and_reduced_tariff(self):
+        start = parse('01-01-2018 21:57:13Z')
+        end = parse('01-01-2018 22:10:56Z')
+        record = CallRecord(started_at=start, ended_at=end,
+                            call_id=uuid.uuid4())
+        record.save()
+
+        assert record.price == 0.36 + 0.09*2

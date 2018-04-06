b935f04b1cdd6b1ab38fe67556c60b44b20c9a94
Diego Ponciano <dgo.ponciano@gmail.com>
2018-04-06 02:06:41 -0300

    Clarifying serializer magic

--- callculator/records/serializers.py
+++ callculator/records/serializers.py
@@ -9,6 +9,11 @@ RECORD_TYPES = [
 ]
 
 
+def unset(obj, attr):
+    if hasattr(obj, attr):
+        delattr(obj, attr)
+
+
 class RecordSerializer(serializers.ModelSerializer):
     call_id = serializers.CharField(validators=[])
     type = serializers.ChoiceField(choices=RECORD_TYPES, write_only=True)
@@ -16,35 +21,33 @@ class RecordSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CallRecord
-        fields = '__all__'
         read_only_fields = ['started_at', 'ended_at', 'duration', 'price']
 
-    def get_extra_kwargs(self, *args, **kwargs):
-        try:
-            if self.initial_data['type'] == 'end':
-                return {
-                    'source': {'read_only': True},
-                    'destination': {'read_only': True}
-                }
-        except (AttributeError, KeyError):
-            pass
-        return super().get_extra_kwargs(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # if call is of type `end` excludes source and destination
+        if getattr(self, 'initial_data', {}).get('type') == 'end':
+            setattr(self.Meta, 'exclude', ['source', 'destination'])
+            unset(self.Meta, 'fields')
+        else:
+            setattr(self.Meta, 'fields', '__all__')
+            unset(self.Meta, 'exclude')
 
     def create(self, validated_data):
         MOMENTS = {
             'start': 'started_at',
             'end': 'ended_at'
         }
-        moment = validated_data.pop('type')
+        moment = MOMENTS[validated_data.pop('type')]
         try:
             record = CallRecord.objects.get(call_id=validated_data['call_id'])
-            setattr(record, MOMENTS[moment], validated_data.pop('timestamp'))
+            setattr(record, moment, validated_data.pop('timestamp'))
             if not record.is_start_before_end():
-                error_msg = 'A call cannot start after the end.'
-                raise serializers.ValidationError(error_msg)
+                error_msg = 'A call cannot have started after it ended.'
+                raise serializers.ValidationError({moment: error_msg})
             return self.update(record, validated_data)
         except CallRecord.DoesNotExist:
-            validated_data[MOMENTS[moment]] = validated_data.pop('timestamp')
+            validated_data[moment] = validated_data.pop('timestamp')
             return CallRecord.objects.create(**validated_data)
 
 
